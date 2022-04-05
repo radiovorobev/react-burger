469243772bad5fec26b1980c6fe7b26e48ac3ce2
Sergey Vorobev <radio.vorobev@yandex.ru>
2022-04-05 13:09:11 +0300

    header

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './App.css';
+import AppHeader from '../AppHeader/AppHeader.jsx';
+
+function App() {
+  return (
+    <div>
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,35 @@
+import headerStyles from './AppHeader.module.css';
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
+function AppHeader() {
+	return (
+
+			<header className={`${headerStyles.header}`}>
+				<div className={`${headerStyles.content} pt-4 pb-4`}>
+					<nav>
+						<ul className={`${headerStyles.nav}`}>
+							<li className={`${headerStyles.button} p-5 mr-2`}>
+								<BurgerIcon type="primary"/>
+								<span className="text text_type_main-default ml-2">Конструктор</span>
+							</li>
+							<li className={`${headerStyles.button} p-5`}>
+								<ListIcon type="secondary"/>
+								<span className="text text_type_main-default text_color_inactive ml-2">Лента заказов</span>
+							</li>
+						</ul>
+					</nav>
+					<Logo />
+					<div className={`${headerStyles.button} p-5`}>
+						<ProfileIcon type="secondary"/>
+						<span className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</span>
+					</div>
+				</div>
+			</header>
+
+	);
+}
+
+export default AppHeader;
