15fd244ff7f0e265fc055532713014a074ae7666
Sergey Vorobev <radio.vorobev@yandex.ru>
2022-06-27 16:04:30 +0300

    dnd installed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,25 +3,21 @@ import AppHeader from '../AppHeader/AppHeader.jsx';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients.jsx';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor.jsx';
 import IngredientDetails from '../IngredientDetails/IngredientDetails';
-import {TotalPriceContext} from '../../services/ingredientsContext.jsx';
 import Modal from '../Modal/Modal.jsx';
 
 import styles from './App.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getIngredients, INGREDIENT_MODAL } from '../../services/actions/actions';
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 function App() {
-
-  const [totalPrice, setTotalPrice] = React.useState(0);
 
   const [isIngredientModal, setIngredientModal] = React.useState(false);
 
   const handleIngredientClick = React.useCallback((item) => {
     dispatch({ type: INGREDIENT_MODAL, ingredient: item });
     setIngredientModal(true);
-    console.log(item);
-    console.log(currentIngredient);
-    console.log(isIngredientModal);
   })
 
   const dispatch = useDispatch();
@@ -35,12 +31,10 @@ function App() {
     <>
       <AppHeader />
       <main className={`${styles.main}`}>
-        <TotalPriceContext.Provider value={{ totalPrice, setTotalPrice }}>
-
+        <DndProvider backend={HTML5Backend}>
             <BurgerIngredients handleIngredientClick={handleIngredientClick}/>
             <BurgerConstructor />
-
-        </TotalPriceContext.Provider>
+        </DndProvider>
       </main>
 
       {isIngredientModal && <Modal onClose={setIngredientModal} title={'Детали ингредиента'} >
